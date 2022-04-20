d44a7b5b39fdabc9e4848772051f9b012a108beb
David <Rigby335@hotmail.com>
2022-04-20 15:39:07 -0700

    Fixed CI issues

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // import functions
-const myName = document.getElementById('my-name');
 const displayAnimalButton = document.getElementById('display-animal-button');
-const displayNameButton = document.getElementById('display-name-button');
 const animalHidden = document.getElementById('animal-hidden');
 // grab DOM elements
 
